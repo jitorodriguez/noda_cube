4de36bb39f25947e124f45e994a1afd9fb2020a7
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-02-23 15:44:39 -0500

    initial project commit that includes imported project files for FirstPerson Prefabs and original assests that simulates demo functionality for game

---
+++ Assets/Rotating.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotating : MonoBehaviour {
+
+	public GameObject cube;
+	public float multiplier = 10f;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDrag(){
+		Debug.Log ("Your draging");
+		cube.transform.Rotate (Input.GetAxis ("Mouse Y") * multiplier, -Input.GetAxis ("Mouse X") * multiplier, 0, Space.World);
+	}
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	 float horizontalSpeed = 10;
+	 float verticleSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h =(horizontalSpeed * Input.GetAxis ("Mouse X"));
+		float v = verticleSpeed * Input.GetAxis ("Mouse Y");
+		transform.Rotate (-v, h, 0);
+	}
+}
