6b605454a73679d0b8672712fa939b5e5b8f585a
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-02-23 23:34:30 -0500

    Replaced old project files with updated ones. Added NodeBehaviour Script to link nodes to parent tree. BUG: Upon turning X axis 180 degrees, Y axis flips resulting in broken rotations

---
+++ Assets/Scripts/NodeBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeBehavior : MonoBehaviour {
+
+	public GameObject cube;
+	public GameObject node1;
+
+	// Use this for initialization
+	void Start () {
+		
+		Vector3 cubeOrigin = cube.transform.localPosition;
+		float scale = cube.transform.localScale.x / 2;
+		//Vector3 newPos = Vector3(cubeOrigin.x, cubeOrigin.y, cubeOrigin.z - scale); 
+		node1.transform.localPosition = new Vector3(cubeOrigin.x, cubeOrigin.y, cubeOrigin.z + scale);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 cubeOrigin = cube.transform.localPosition;
+		float scale = cube.transform.localScale.x / 2;
+		//Vector3 newPos = Vector3(cubeOrigin.x, cubeOrigin.y, cubeOrigin.z - scale); 
+		node1.transform.localPosition = new Vector3(cubeOrigin.x, cubeOrigin.y, cubeOrigin.z + scale);
+
+	}
+}
---
+++ Assets/Scripts/Rotating.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotating : MonoBehaviour {
+
+	public GameObject cube;
+	//public GameObject node1;
+	public float multiplier = 10f;
+	// Use this for initialization
+	void Start () {
+
+		//Get Location of Cube
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDrag(){
+
+		Debug.Log ("Your draging");
+		cube.transform.Rotate (Input.GetAxis ("Mouse Y") * multiplier, -Input.GetAxis ("Mouse X") * multiplier, 0, Space.World);
+		
+	}
+}
